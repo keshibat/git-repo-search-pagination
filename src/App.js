c4ad84c519e396e083d283fecfa68a460b01d6e0
keshibat <kensukeshibata@gmail.com>
2022-10-06 17:28:31 +0900

    added search function for git repository

--- src/App.js
+++ src/App.js
@@ -1,49 +1,72 @@
 import { useState, useMemo } from 'react'
 import Pagination from './components/pagination/pagination.component'
-import data from './data/mock-data.json'
 import './styles.scss'
 
 let PageSize = 10
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState(1)
+  const [ repos, setRepos ] = useState([])
+  const [ searchInput, setSearchInput ] = useState('')
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    if(!searchInput) {
+      alert('please enter some keywords')
+      return
+    }
+    const data = await fetch(`https://api.github.com/search/repositories?q=${searchInput}+in:name&sort=stars`)
+    const repositories = await data.json()
+    console.log(repositories.items)
+    if(data) {
+      setRepos(repositories.items)
+    }
+  }
 
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize
     const lastPageIndex = firstPageIndex + PageSize
-    return data.slice(firstPageIndex, lastPageIndex)
-  }, [currentPage])
+    return repos.slice(firstPageIndex, lastPageIndex)
+  }, [repos, currentPage])
 
   return (
     <>
+      <div>
+        <input type="text" placeholder="search repositories" value={searchInput} onChange={handleChange}/>
+        <button type="submit" onClick={submitHandler}>Search</button>
+      </div>
       <table>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>FIRST NAME</th>
-            <th>LAST NAME</th>
-            <th>EMAIL</th>
-            <th>PHONE</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>fork</th>
+            <th>language</th>
+            <th>Update At</th>
           </tr>
         </thead>
-        <tbody>
-          {currentTableData.map(item => {
+        { <tbody>
+          {currentTableData && currentTableData.map(repo => {
             return (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
+              <tr key={repo.id}>
+                <td><a href={repo.clone_url}>{repo.name}</a></td>
+                <td>{repo.description}</td>
+                <td>{repo.forks_count}</td>
+                <td>{repo.language}</td>
+                <td>{repo.updated_at}</td>
               </tr>
             );
           })}
-        </tbody>
+        </tbody> }
       </table>
       <Pagination
         className="pagination-bar"
         currentPage={currentPage}
-        totalCount={data.length}
+        totalCount={repos.length}
         pageSize={PageSize}
         onPageChange={page => setCurrentPage(page)}
       />
